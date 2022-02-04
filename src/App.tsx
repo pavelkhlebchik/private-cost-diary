f819d867a5ab37f003f883fa33aeadc780dbe373
Pavel Koltsov <pavelkhlebchik@gmail.com>
2022-02-04 13:32:45 +0300

    [ADD] roouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
 
 import React, { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './routes/HomePage/HomePage';
+import NewCardPage from './routes/NewCardPage/NewCardPage';
 
-const App: FC = () => <div />;
+const App: FC = () => (
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/home" element={<HomePage />} />
+    {/* <Route path="/all" element={<TasksPage />} />
+    <Route path="/new" element={<NewCardPage />} />
+    */}
+    <Route path="/new" element={<NewCardPage />} />
+  </Routes>
+);
+
+// тут мы делаем роуты
 
 export default App;
